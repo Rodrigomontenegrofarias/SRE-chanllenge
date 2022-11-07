260d5a96b8822b0367828b172581136b35d461d1
Rodrigomontenegrofarias <rodrigo.montenegro@alumnso.uv.cl>
2022-11-07 11:03:41 -0300

    subida de terraform para la automatizacion

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 2.13.0"
     }
   }
@@ -9,16 +9,30 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "notebook-prueba" {
-  name         = "monteblack1/notebook-prueba:latest"
+resource "docker_image" "notebook-jupyter" {
+  name         = "monteblack1/notebook-jupyter:latest"
   keep_locally = false
 }
 
-resource "docker_container" "notebook-prueba" {
-  image = docker_image.notebook-prueba.latest
-  name  = "tutorial"
+resource "docker_container" "notebook-jupyter" {
+  image = docker_image.notebook-jupyter.latest
+  name  = "tutoriall"
   ports {
     internal = 8888
     external = 8886
+  }
+}
+
+resource "docker_image" "api" {
+  name         = "monteblack1/api:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "api" {
+  image = docker_image.api.latest
+  name  = "tutorial1"
+  ports {
+    internal = 5000
+    external = 5000
   }
 }
